bacc1eb671a13000360ba5935d70637a4746eb13
Alon Ben Refael <alonbr@proton.me>
2025-06-25 09:18:44 +0300

    Update app.js

--- templates/app.js
+++ templates/app.js
@@ -18,7 +18,7 @@ function next_page(q_or_a){
 		}
 		else{
 			text += getNow() + ',completed\\r\\n';
-			console.log(text);
+//			console.log(text);
 			//downloadData(text,filename);
 			location.replace('Done.html');
 		}
@@ -56,35 +56,35 @@ function downloadData(data, filename) {
 	document.body.removeChild(element);
 }
 
-(function(console){
+//(function(console){
 
-	console.save = function(data, filename){
+//	console.save = function(data, filename){
 
-		if(!data) {
-			console.error('Console.save: No data')
-			return;
-		}
- 
+//		if(!data) {
+//			console.error('Console.save: No data')
+//			return;
+//		}
+// 
 
-		if(!filename) filename = 'console.json'
+//		if(!filename) filename = 'console.json'
 
-		if(typeof data === "object"){
-			data = JSON.stringify(data, undefined, 4)
-		}
+//		if(typeof data === "object"){
+//			data = JSON.stringify(data, undefined, 4)
+//		}
 
-		var blob = new Blob([data], {type: 'text/json'}),
-			e = document.createEvent('MouseEvents'),
-			a = document.createElement('a')
+//		var blob = new Blob([data], {type: 'text/json'}),
+//			e = document.createEvent('MouseEvents'),
+//			a = document.createElement('a')
 
-		a.download = filename
-		a.href = window.URL.createObjectURL(blob)
-		a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
-		e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-		a.dispatchEvent(e)
+//		a.download = filename
+//		a.href = window.URL.createObjectURL(blob)
+//		a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+//		e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+//		a.dispatchEvent(e)
 
 
-	}
-})(console)
+//	}
+//})(console)
 
 
 function getNow() {
