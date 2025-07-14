441568449af5a822de23b608437ab5d07e265b49
Alon Ben Refael <alonbr@proton.me>
2025-07-14 08:12:35 +0300

    Update PowerMod_builder.py

--- PowerMod_builder.py
+++ PowerMod_builder.py
@@ -48,6 +48,7 @@ class MyHandler(SimpleHTTPRequestHandler):
 			cpit(os.path.join(src,n_or_m + '.css'),os.path.join(cwd,'css','powermod.css'))
 			cpit(os.path.join(src,'Done.html'),os.path.join(cwd,'done','Done.html'))
 			cpit(os.path.join(src,'hint.png'),os.path.join(cwd,'img','hint.png'))
+			cpit([src,'video-js-8.23.3'],[cwd,'scripts','video-js-8.23.3'])
 			app_js = io.open(os.path.join(src,'app.js'), mode="r",encoding="utf-8").read()
 			if data['keep_data'] == 'yes' :
 				app_js = app_js.replace('//', '')
